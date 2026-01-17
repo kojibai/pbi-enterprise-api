4a0d55e54204556af996a4275e0edf3699d5cc00
Kojib <kojibchat@gmail.com>
2026-01-17 02:53:03 -0500

    update favicons

--- apps/portal/.next/static/chunks/webpack.js
+++ apps/portal/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9321a733497958f7"; }
+/******/ 		__webpack_require__.h = function() { return "892f0a2466646616"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
